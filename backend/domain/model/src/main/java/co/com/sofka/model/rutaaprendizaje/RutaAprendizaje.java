93dab8047cc941be5e7871b32b99dc86fb7a61fd
SantiagoOspinaO <santiago.m200@outlook.es>
2022-08-05 19:12:02 -0500

    Se crea el paquete del modelo de ruta aprendizaje

---
+++ backend/domain/model/src/main/java/co/com/sofka/model/rutaaprendizaje/RutaAprendizaje.java
@@ -0,0 +1,8 @@
+package co.com.sofka.model.rutaaprendizaje;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder(toBuilder = true)
+public class RutaAprendizaje {
+}
---
+++ backend/domain/model/src/main/java/co/com/sofka/model/rutaaprendizaje/gateways/RutaAprendizajeRepository.java
@@ -0,0 +1,4 @@
+package co.com.sofka.model.rutaaprendizaje.gateways;
+
+public interface RutaAprendizajeRepository {
+}
