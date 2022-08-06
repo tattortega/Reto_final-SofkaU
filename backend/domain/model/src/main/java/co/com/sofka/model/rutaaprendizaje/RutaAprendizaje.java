110102fde8c504373cf125ab598bded075fac119
SantiagoOspinaO <santiago.m200@outlook.es>
2022-08-05 22:28:15 -0500

    Se crea el modelo de ruta de aprendizaje

--- backend/domain/model/src/main/java/co/com/sofka/model/rutaaprendizaje/RutaAprendizaje.java
+++ backend/domain/model/src/main/java/co/com/sofka/model/rutaaprendizaje/RutaAprendizaje.java
@@ -1,8 +1,29 @@
 package co.com.sofka.model.rutaaprendizaje;
-import lombok.Builder;
-import lombok.Data;
+
+import lombok.*;
 
 @Data
 @Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class RutaAprendizaje {
+
+    private String id;
+    private String nombre;
+    private String descripcion;
+    private Integer nivel;
+    //private Curso curso;
+    private String prerrequisito;
+
+    @Override
+    public String toString() {
+        return "RutaAprendizaje{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", nivel=" + nivel +
+                //", curso=" + curso +
+                ", prerrequisito='" + prerrequisito + '\'' +
+                '}';
+    }
 }
--- backend/domain/model/src/main/java/co/com/sofka/model/rutaaprendizaje/gateways/RutaAprendizajeRepository.java
+++ backend/domain/model/src/main/java/co/com/sofka/model/rutaaprendizaje/gateways/RutaAprendizajeRepository.java
@@ -1,4 +1,16 @@
 package co.com.sofka.model.rutaaprendizaje.gateways;
 
+import co.com.sofka.model.rutaaprendizaje.RutaAprendizaje;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface RutaAprendizajeRepository {
+
+    Mono<RutaAprendizaje> save(RutaAprendizaje rutaAprendizaje);
+
+    Flux<RutaAprendizaje> findAll();
+
+    Mono<RutaAprendizaje> update(String id, RutaAprendizaje rutaAprendizaje);
+
+    Mono<Void> deleteById(String id);
 }
